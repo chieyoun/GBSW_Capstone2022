cdbae44e147f1f442f5333b34b0db40ceeff7228
hanchaeyoung <gks4167@naver.com>
2022-06-30 17:52:39 +0900

    qwe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Example() {
   return (
     <Disclosure as="nav" className="bg-Zinc-900">
       {({ open }) => (
-        <>
+        <div>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -147,7 +147,7 @@ export default function Example() {
             ))}
           </div>
         </Disclosure.Panel>
-      </>
+      </div>
     )}
   </Disclosure>
 )
