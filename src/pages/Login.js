f39eee9fcc904ef26dbe5d6627444ad48cd6dd84
ksh1117 <rlatjdgml5916@naver.com>
2022-07-11 18:21:56 +0900

    iverybingbong

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,58 +1,42 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import '../styles/login.css';
 
-import '../styles/Login.css';
 const Login = () => {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
 
-  const onIdHandler = (e) => {
-    setId(e.target.value);
-  }
-  const onpwHandler = (e) => {
-    setPw(e.target.value);
-  }
-
-  // useEffect(() => {
-  //   loadData();
-  // }, [])
-
-  const loadData = async (e) => {
-    // e.preventDefault();
-    
-    let data = {
-      user : "",
+    const onIdHandler = (e) => {
+        setId(e.target.value);
+    }
+    const onpwHandler = (e) => {
+        setPw(e.target.value);
     }
 
-    axios.post("http://127.0.0.1:3307/login", data, { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-      })
+    // useEffect(() => {   loadData(); }, [])
 
-  }
+    const loadData = async (e) => {
+        // e.preventDefault();
 
-  return(
-    <div className="container">
-      <div className="form_container">
-      <div className="left">
-        <h1>LearnChart</h1>
-        <span>이 웹사이트 쓰지마유</span>
-      </div>
-      <div className="right">
-        <div className="right_login">
-        <input type="text" value={id} onChange = {onIdHandler} placeholder = "아이디" />
-        <input type="password" value={pw} onChange = {onpwHandler} placeholder = "비밀번호" />
-        <button type="submit" onClick={(e) => loadData()}>로그인</button>
-        <span>계정이 없으신가요?</span>
-        <Link to='/signup'>
-        <span>회원가입</span>
-        </Link>
+        let data = {
+            user: ""
+        }
+        axios
+            .post("http://127.0.0.1:3307/login", data, {withCredentials: true})
+            .then((res) => {
+                console.log(res);
+            })
+
+    }
+
+    return (
+        <div>
+            <div className="Container">
+                
+            </div>
         </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Login
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, {Profiler, useState} from "react";
 import '../styles/Login_Signup.css';
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
     const [id, setId] = useState('');
@@ -26,7 +27,7 @@ const Signup = () => {
     };
     const onBtnClick = (e) => {
         if (pw !== pwCheck) {
-            alert("비밀번호를 다시 확인 해주세요");
+            alert("비밀번호를 다시 확인 해주세요"); 
         } else {
             console.log("이름: " + name);
             console.log("아이디: " + id);
@@ -39,7 +40,6 @@ const Signup = () => {
                 password : pw,
                 birthday: bdate,
             }
-
             axios.post("/api/auth/join", body, {withCredentials: true})
             .then((res) => {
                 console.log(res);
@@ -53,7 +53,7 @@ const Signup = () => {
     return (
         <div className="center">
             <div className="signppage">
-                <form>
+                <form onSubmit={onBtnClick}>
                     <h2>회원가입</h2>
                     <div>
                         <input className="Name" type="text" value={name} onChange={onNameHandler} placeholder="*이름"/><br/>
@@ -66,7 +66,9 @@ const Signup = () => {
                             onChange={onIdHandler}
                             minLength='5'
                             maxLength='16'
-                            placeholder="*아이디(5~16자)"/><br/>
+                            placeholder="*아이디(5~16자)"
+                            required
+                            /><br/>
                     </div>
                     <div>
                         <input
@@ -75,6 +77,7 @@ const Signup = () => {
                             value={pw}
                             onChange={onPwHandler}
                             maxLength='12'
+                            required
                             placeholder="*비밀번호(1~12자)"/><br/>
                     </div>
                     <div>
@@ -84,13 +87,18 @@ const Signup = () => {
                             value={pwCheck}
                             onChange={onCpwHandler}
                             maxLength='12'
+                            required
                             placeholder="*비밀번호 확인"/><br/>
                     </div>
                     <div>
-                        <input type="text" placeholder="생년월일(ex-2005.01.01)" value={bdate} onChange={onBdateHandler}/>
+                        <input required type="text" placeholder="생년월일(ex-2005.01.01)" value={bdate} onChange={onBdateHandler}/>
                     </div>
                     <div>
-                        <button type="submit" onClick={onBtnClick}>회원 가입</button>
+                        <button type="submit" >회원 가입</button>
+                        <p>계정이 있으신가요?</p>
+                        <Link to='/'>
+                            <p>로그인</p>
+                        </Link>
                     </div>
                 </form>
             </div>
